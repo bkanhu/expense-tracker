21aebbc5a455dffcff9bedcc3c865569cefda039
B Kanhu Charan <iamkanhuc@gmail.com>
2023-12-16 00:45:45 +0530

    View payment mehtod by individual user done

--- src/app/api/payments-methods/route.js
+++ src/app/api/payments-methods/route.js
@@ -25,29 +25,27 @@ export async function POST(request) {
   }
 }
 
-// // API Route to get all the cateogries by a user from Mongodb Collections
-// export async function GET(request) {
-//   const client = await clientPromise;
+// API Route to get all the payment Methods added by a user from Mongodb Collections
+export async function GET(request) {
+  const client = await clientPromise;
 
-//   const db = client.db('expenseTracker');
-//   const collection = db.collection('categories');
-//   // const { uid } = await request.query;
+  const db = client.db('expenseTracker');
+  const collection = db.collection('paymentMethods');
+  // const { uid } = await request.query;
 
-//   // const url = new URL(request.url);
-//   // console.log('request', request.nextUrl);
-//   const searchParams = request.nextUrl.searchParams;
-//   const uid = searchParams.get('uid');
+  // const url = new URL(request.url);
+  // console.log('request', request.nextUrl);
+  const searchParams = request.nextUrl.searchParams;
+  const uid = searchParams.get('uid');
 
-//   console.log('uid', uid);
+  console.log('uid', uid);
 
-//   try {
-//     // const categories = await collection.find({}).toArray();
-//     // const categories = await collection.find({ uid }).toArray();
-//     const categories = await collection.find({ uid: uid }).toArray();
-//     // const categories = { hello: 'world' };
-//     return NextResponse.json(categories);
-//   } catch (error) {
-//     console.error('Error in API route:', error);
-//     return NextResponse.error({ status: 500, body: 'Internal Server Error' });
-//   }
-// }
+  try {
+    const paymentMethods = await collection.find({ uid: uid }).toArray();
+    // const paymentMethods = { hello: 'world' };
+    return NextResponse.json(paymentMethods);
+  } catch (error) {
+    console.error('Error in API route:', error);
+    return NextResponse.error({ status: 500, body: 'Internal Server Error' });
+  }
+}
